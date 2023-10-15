0992ab04fb1fea6f29ced98de84c18b11e97b649
rysntkang <132887121+rysntkang@users.noreply.github.com>
2023-10-15 16:43:47 +0800

    Changed the object construction to be in the controller instead of the entity

--- bndProj/entities/user_Class.php
+++ bndProj/entities/user_Class.php
@@ -2,23 +2,6 @@
 require("../dbConnection.php");
 
 class User {
-    public $firstName;
-    public $lastName;
-    public $username;
-    public $password;
-    public $mobileNumber;
-    public $userProfileId;
-
-    public function __construct($firstName, $lastName, $username, $password, $mobileNumber, $userProfileId) {
-  		$this->userId = $userId;
-        $this->firstName = $firstName;
-        $this->lastName = $lastName;
-        $this->username = $username;
-        $this->password = $password;
-        $this->mobileNumber = $mobileNumber;
-        $this->userProfileId = $userProfileId;
-    }
-
 	public static function login($conn, $username, $password) {
 	$username = $conn->real_escape_string($username);
 	$password = $conn->real_escape_string($password);
