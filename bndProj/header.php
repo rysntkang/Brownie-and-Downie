912e0120054632dcd7b62c5fa932413a8dd21e09
rysntkang <132887121+rysntkang@users.noreply.github.com>
2023-10-17 09:29:44 +0800

    Added back plugins and implemented easier way to include them for front end

---
+++ bndProj/header.php
@@ -0,0 +1,12 @@
+<!-- Remember to change the base url -->
+<?php $base_url = "http://localhost/bndProj/"; ?>
+
+<head>
+    <link rel="stylesheet" href="<?php echo $base_url ?>plugins\Bootstrap 4\css\bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo $base_url ?>plugins/fontawesome-free/css/all.min.css">
+
+    <script src="<?php echo $base_url ?>plugins\Bootstrap 4\js\bootstrap.min.js"></script>
+    <script src="<?php echo $base_url ?>plugins\jquery\jquery.min.js"></script>
+    <script src="<?php echo $base_url ?>plugins\popper\popper.min.js"></script>
+
+</head>
--- bndProj/main/login.php
+++ bndProj/main/login.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" class="" style="height: auto;">
 <?php require_once('../config.php') ?>
+<?php include("../../header.php") ?>
 
 <?php
 
@@ -35,13 +36,6 @@ function redirectHomePage($userProfileId){
 
 }
 ?>
-
-<head>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://kit.fontawesome.com/f2f42d264c.js" crossorigin="anonymous"></script>
-</head>
 
 <body>
 <style>
