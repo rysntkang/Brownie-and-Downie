8ff5de30ce35898258a4c4eee8a5266bbe31ec57
rysntkang <132887121+rysntkang@users.noreply.github.com>
2023-10-21 08:09:11 +0800

    Updated Side Nav Bar

--- bndProj/main/admin/sideNavBar.php
+++ bndProj/main/admin/sideNavBar.php
@@ -6,16 +6,17 @@
   <div class="sidebar">
     <nav class="mt-4">
       <ul class="nav nav-pills nav-sidebar flex-column text-sm nav-compact nav-flat" role="menu">
+        <li class="nav-header">User Profiles</li>
         <li class="nav-item">
-          <a href="./" class="nav-link nav-home">
-            <i class="nav-icon fas fa-tachometer-alt"></i>
-            <p> Example 1 </p>
+          <a href="index.php?page=viewUserProfileBoundary" class="nav-link nav-home">
+            <i class="nav-icon fas fa-address-card active"></i>
+            <p>View All User Profiles</p>
           </a>
         </li>
-        <li class="nav-item dropdown">
-          <a href="./" class="nav-link nav-home">
-            <i class="nav-icon fas fa-tachometer-alt"></i>
-            <p> Example 2 </p>
+        <li class="nav-item">
+          <a href="index.php?page=createUserProfileBoundary" class="nav-link nav-home">
+            <i class="nav-icon fas fa-address-card"></i>
+            <p>Create User Profile</p>
           </a>
         </li>
         <li class="nav-header">Example Header</li>
